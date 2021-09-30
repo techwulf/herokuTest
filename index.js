3383383c2510879f7a68abd21938e92e5a823937
techwulf <t.zach.moore@gmail.com>
2021-09-30 11:50:06 -0600

    bring in react

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
 const express = require('express');
-
 const server = express();
+const path = require('path');
 
 server.use(express.json());
+server.use(express.static(
+  path.join(__dirname, 'client/build')
+));
 
-server.get('/', (req, res) => {
-  res.send('<h1>Main Page!!</h1>');
+server.get('*', (req, res) => {
+  res.sendFile(
+    path.join(__dirname, 'client/build', 'index.html')
+  );
 });
 
 server.get('/hello', (req, res) => {
